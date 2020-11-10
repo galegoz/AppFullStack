e8e037e5e1441eaa787ebc6e4508da597a1fc12e
Lucas Carvalho <llucassduarte50@gmail.com>
2020-11-10 20:00:59 -0300

    GET records

--- backend/src/main/java/com/lucas/dspesquisa/resources/RecordResources.java
+++ backend/src/main/java/com/lucas/dspesquisa/resources/RecordResources.java
@@ -1,10 +1,18 @@
 package com.lucas.dspesquisa.resources;
 
+import java.time.Instant;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lucas.dspesquisa.dto.RecordDto;
@@ -23,5 +31,27 @@ public class RecordResources {
 		RecordDto newDTO = service.insert(dto);
 		return ResponseEntity.ok().body(newDTO);
 	}
-
+	
+	@GetMapping
+	public ResponseEntity<Page<RecordDto>> findAll(
+			@RequestParam(value = "min", defaultValue = "") String min,
+			@RequestParam(value = "max", defaultValue = "") String max,
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "0") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "moment") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "DESC") String direction) {
+		
+		Instant minDate = ("".equals(min)) ? null : Instant.parse(min);
+		Instant maxDate = ("".equals(max)) ? null : Instant.parse(max);
+		
+		if (linesPerPage == 0) {
+			linesPerPage = Integer.MAX_VALUE;
+		}
+		
+		PageRequest pageRequest = PageRequest.of(page, 
+				linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		Page<RecordDto> list = service.findByMoments(minDate, maxDate, pageRequest);
+		return ResponseEntity.ok().body(list);
+	}
 }
--- backend/src/main/java/com/lucas/dspesquisa/services/RecordServices.java
+++ backend/src/main/java/com/lucas/dspesquisa/services/RecordServices.java
@@ -1,8 +1,11 @@
 package com.lucas.dspesquisa.services;
 
 import java.time.Instant;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,8 +13,8 @@ import com.lucas.dspesquisa.dto.RecordDto;
 import com.lucas.dspesquisa.dto.RecordInsertDTO;
 import com.lucas.dspesquisa.entities.Game;
 import com.lucas.dspesquisa.entities.Record;
-import com.lucas.dspesquisa.repositories.RecordRepository;
 import com.lucas.dspesquisa.repositories.GameRepository;
+import com.lucas.dspesquisa.repositories.RecordRepository;
 
 @Service
 public class RecordServices {
@@ -34,11 +37,14 @@ public class RecordServices {
 		Game game = gameRepository.getOne(dto.getGameId());
 		entity.setGame(game);
 		
-		entity = repository.save(entity);
-		
+		entity = repository.save(entity);		
 		return new RecordDto(entity);
 	}
-	
-	
 
+
+	@Transactional(readOnly = true)
+	public Page<RecordDto> findByMoments(Instant minDate, Instant maxDate, PageRequest pageRequest) {	
+		return repository.findByMoments(minDate, maxDate, pageRequest).map(x -> new RecordDto(x));
+	}
+		
 }
